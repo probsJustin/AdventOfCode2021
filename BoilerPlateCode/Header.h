58c89acd7a4c6538b3cd982d221c8ffe1491ad2d
Justin Hagerty <justin.hagerty@dynatrace.com>
2021-12-02 12:04:53 -0500

    day2 done

--- BoilerPlateCode/Header.h
+++ BoilerPlateCode/Header.h
@@ -1,1 +1,30 @@
 #pragma once
+#include <string>
+#include <vector>
+
+
+std::vector<int> vectorStringSplitToInt(std::string stringToSplit, std::string delimiter) {
+	std::vector<int> returnVector;
+	size_t pos = 0;
+	std::string token;
+	while ((pos = stringToSplit.find(delimiter)) != std::string::npos) {
+		token = stringToSplit.substr(0, pos);
+		returnVector.push_back(std::stoi(token));
+		stringToSplit.erase(0, pos + delimiter.length());
+	}
+	returnVector.push_back(std::stoi(stringToSplit));	
+	return returnVector;
+}
+
+std::vector<std::string> vectorStringSplitToString(std::string stringToSplit, std::string delimiter) {
+	std::vector<std::string> returnVector;
+	size_t pos = 0;
+	std::string token;
+	while ((pos = stringToSplit.find(delimiter)) != std::string::npos) {
+		token = stringToSplit.substr(0, pos);
+		returnVector.push_back(token);
+		stringToSplit.erase(0, pos + delimiter.length());
+	}
+	returnVector.push_back(stringToSplit);
+	return returnVector;
+}
--- Day2/Day2/Day2.cpp
+++ Day2/Day2/Day2.cpp
@@ -5,7 +5,7 @@
 #include "Header.h"
 #include <fstream>
 #include <streambuf>
-
+#include <map>
 using namespace std;
 
 string getPuzzleText(string fileLocation) {
@@ -22,9 +22,68 @@ string getPuzzleText(string fileLocation) {
 	return str; 
 }
 
+void part1(vector<string> inputVector) {
+	vector<string> parsedPuzzle = inputVector;
+	vector<vector<string>> fullParsedPuzzle;
 
+	map<string, int> position; 
+	position["forward"] = 0; 
+	position["up"] = 0; 
+	position["down"] = 0; 
+
+	for (int i = 0; i < parsedPuzzle.size(); i++) {
+		fullParsedPuzzle.push_back(vectorStringSplitToString(parsedPuzzle[i], { ' ' }));
+	}
+
+	for (int x = 0; x <= fullParsedPuzzle.size() - 2; x++) {
+		for (int y = 0; y <= fullParsedPuzzle[x].size() - 2; y = y + 2) {
+				position[fullParsedPuzzle[x][y]] = position[fullParsedPuzzle[x][y]] + stoi(fullParsedPuzzle[x][y + 1]);
+		}
+	}
+
+	cout << "Part1 Answer:"  << (position["down"] - position["up"]) * position["forward"] << endl;
+}
+
+void part2(vector<string> inputVector) {
+
+	map<string, int> position;
+	position["aim"] = 0;
+	position["depth"] = 0;
+	position["horizontal"] = 0;
+
+	vector<string> parsedPuzzle = inputVector;
+	vector<vector<string>> fullParsedPuzzle;
+	for (int i = 0; i < parsedPuzzle.size(); i++) {
+		fullParsedPuzzle.push_back(vectorStringSplitToString(parsedPuzzle[i], { ' ' }));
+	}
+
+	for (int x = 0; x <= fullParsedPuzzle.size() - 2; x++) {
+		for (int y = 0; y <= fullParsedPuzzle[x].size() - 2; y = y + 2) {
+			switch (fullParsedPuzzle[x][y][0]) {
+				case 'u': {
+					position["aim"] = position["aim"] - stoi(fullParsedPuzzle[x][y + 1]);
+					break;
+				}
+				case 'd': {
+					position["aim"] = position["aim"] + stoi(fullParsedPuzzle[x][y + 1]);
+					break;
+				}
+				case 'f': {
+					position["horizontal"] = position["horizontal"] + stoi(fullParsedPuzzle[x][y + 1]);
+					position["depth"] = position["depth"] + (position["aim"] * stoi(fullParsedPuzzle[x][y + 1]));
+					break;
+				}
+			}
+
+		}
+	}
+	cout << "Part2 Answer:" << position["horizontal"] * position["depth"] << endl;
+
+}
 
 int main() {
+	part1(vectorStringSplitToString(getPuzzleText("./puzzle.txt"), { '\n' }));
+	part2(vectorStringSplitToString(getPuzzleText("./puzzle.txt"), { '\n' }));
 
 	system("pause");
 	return 0;
